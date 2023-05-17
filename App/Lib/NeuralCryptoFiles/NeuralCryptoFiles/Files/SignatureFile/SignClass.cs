bec1515377aee73b97143cdbcdd10a02cdcf4f5b
CadityAgent <110487980+CadityAgent@users.noreply.github.com>
2023-05-17 20:25:01 +0200

    Ajout des classes pour les fichiers, lib et autres

---
+++ App/Lib/NeuralCryptoFiles/NeuralCryptoFiles/Files/SignatureFile/SignClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NeuralCryptoFiles.Files.SignatureFile
+{
+	public class SignClass
+	{
+		private SignFileParameters parameters { get; }
+
+		public SignClass(SignFileParameters inputParam)
+		{
+			parameters = inputParam;
+        }
+	}
+
+	public class SignFileParameters
+	{
+		public List<string> filePath { get; set; }
+		public DateTime expirationDate { get; set; }
+		public string author { get; set; }
+		public string privateKey { get; set; }
+		public string passwordKey { get; set; }
+		public string userComment { get; set; }
+		public string fileName { get; set; }
+		public string copyFilePath { get; set; }
+		public string outputPath { get; set; }
+	}
+}
+
